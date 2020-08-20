5e2ddb7d49dcaa5ed97ef679f573ad139c71f3d4
William Pavei Antero <williamantero@gmail.com>
2020-08-20 00:48:18 -0300

    adjust numpy import

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-import numpy as np
+import numpy
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
