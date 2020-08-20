a1bbfef35339810a6426c5f86bd3b5f5c97b81f6
William Pavei Antero <williamantero@gmail.com>
2020-08-19 22:28:40 -0300

    fix MeanGradeIfNaN and add MeanGrades

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -59,12 +59,37 @@ class MeanGradeIfNaN(BaseEstimator, TransformerMixin):
     def transform(self, X):
         data = X.copy()
 
-        data['NOTA_DE'] = data['NOTA_DE'].apply(lambda x: data['NOTA_EM'] if np.isnan(x) else x)
-        data['NOTA_EM'] = data['NOTA_EM'].apply(lambda x: data['NOTA_DE'] if np.isnan(x) else x)
-        data['NOTA_MF'] = data['NOTA_MF'].apply(lambda x: data['NOTA_GO'] if np.isnan(x) else x)
-        data['NOTA_GO'] = data['NOTA_GO'].apply(lambda x: data['NOTA_MF'] if np.isnan(x) else x)
+        for index, row in data.iterrows():
+            if(type(row['NOTA_DE']) == float and pd.isna(row['NOTA_DE'])):
+                data.loc[data.index == index, 'NOTA_DE'] = row['NOTA_EM']
+            if(type(row['NOTA_EM']) == float and pd.isna(row['NOTA_EM'])):
+                data.loc[data.index == index, 'NOTA_EM'] = row['NOTA_DE']
+            if(type(row['NOTA_MF']) == float and pd.isna(row['NOTA_MF'])):
+                data.loc[data.index == index, 'NOTA_MF'] = row['NOTA_GO']
+            if(type(row['NOTA_GO']) == float and pd.isna(row['NOTA_GO'])):
+                data.loc[data.index == index, 'NOTA_GO'] = row['NOTA_MF']
         
-        return data        
+        return data
+
+class MeanGrades(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        return
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        data = X.copy()
+
+        for index, row in data.iterrows():
+            meanExact = round((row['NOTA_DE'] + row['NOTA_EM']) / 2, 1)
+            menaHuman = round((row['NOTA_MF'] + row['NOTA_GO']) / 2, 1)
+            data.loc[data.index == index, 'NOTA_DE'] = meanExact
+            data.loc[data.index == index, 'NOTA_EM'] = meanExact
+            data.loc[data.index == index, 'NOTA_MF'] = menaHuman
+            data.loc[data.index == index, 'NOTA_GO'] = menaHuman
+        
+        return data
     
 class UpdateFeatures(BaseEstimator, TransformerMixin):
     def __init__(self,features):
