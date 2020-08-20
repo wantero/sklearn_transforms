5583dda621d950fdcdb8ac3dde4d24300c1c6e63
William Pavei Antero <williamantero@gmail.com>
2020-08-20 19:41:22 -0300

    add panda library

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,6 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 import numpy
+import pandas as pd
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
